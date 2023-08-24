2d2d44a47200a55748afa4a8729dd5711d1f1290
superlion3 <melvinchen610@gmail.com>
2023-08-23 23:35:15 -0400

    added dynamic scroll speed and fixed background

--- src/components/PlayPage.tsx
+++ src/components/PlayPage.tsx
@@ -2,8 +2,13 @@ import "./PlayPage.css";
 import { useState } from "react";
 import classNames from "classnames";
 import { Button } from "./Buttons";
+import CSS from "csstype";
 
 const letters: string[] = "ABCDEFGHIJK".split("");
+const duration = 10;
+const durationStyle: CSS.Properties = {
+    animationDuration: String(duration) + "s",
+};
 
 function shuffle(list: string[]) {
     return list
@@ -20,30 +25,39 @@ function newletterList() {
     return newLetters;
 }
 
+function checkTime() {
+    console.log("HIIIIIIIII");
+    console.timeEnd("hi");
+    return "hi";
+}
+
 function slotCol(list: string[], startTime: DOMHighResTimeStamp) {
     const [stopped, setStopped] = useState(false);
+
     return (
         <div
+            style={durationStyle}
+            onClick={() => {
+
+                // ms/letter - elapsed % ms/letter
+                let waitTime =
+                    (duration * 1000) / letters.length -
+                    ((performance.now() - startTime) %
+                        ((duration * 1000) / letters.length));
+                setTimeout(() => setStopped(true), waitTime);
+                console.log(waitTime);
+                console.time("hi"); 
+            }}
             className={classNames(
                 "slot-container",
                 { stopped: stopped },
                 { started: startTime > 0 }
-            )
-            
-        }
-  
-            onClick={() => {
-                setTimeout(
-                    () => setStopped(true),
-                    10000 / letters.length -
-                        ((performance.now() - startTime) %
-                            (10000 / letters.length))
-                );
-            }}
+            )}
+            key={checkTime()}
         >
             {list.map((letter, index) => (
                 <img
-                    src={"images/" + letter + ".svg"}
+                    src={"images/letters/" + letter + ".svg"}
                     alt={"This was the letter " + { letter }}
                     key={index}
                 />
@@ -64,7 +78,7 @@ export default function PlayPage() {
             <title>Play</title>
             <div>
                 <div className="machine">
-                    <span className="bg"><img src="images/machine-bg.png"/></span>
+                    <span className="bg" />
                     <div className="slot left">
                         {slotCol(leftList, startTime)}
                     </div>
@@ -75,7 +89,6 @@ export default function PlayPage() {
                         {slotCol(rightList, startTime)}
                     </div>
                     <div className="line" />
-
                 </div>
                 {startTime === 0 && (
                     <Button
@@ -85,7 +98,6 @@ export default function PlayPage() {
                     />
                 )}
             </div>
-
         </>
     );
 }
