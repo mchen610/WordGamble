342702e99bc921507af25f268da0c7700291f71c
Melvin Chen <melvinchen610@gmail.com>
2023-08-28 04:30:16 -0400

    fixed hook usage

--- src/components/PlayPage.tsx
+++ src/components/PlayPage.tsx
@@ -5,19 +5,6 @@ import { Button } from "./Buttons";
 
 const letters: string[] = "ABCDEFGHIJK".split("");
 const duration = 2;
-const [listDict, setListDict] = useState({
-    left: newletterList(),
-    center: newletterList(),
-    right: newletterList(),
-});
-
-const [finalLeft, setFinalLeft] = useState(listDict["left"].slice(0, 3));
-
-const [finalCenter, setFinalCenter] = useState(listDict["center"].slice(0, 3));
-
-const [finalRight, setFinalRight] = useState(listDict["right"].slice(0, 3));
-
-
 
 function shuffle(list: string[]) {
     return list
@@ -34,68 +21,93 @@ function newletterList() {
     return newLetters;
 }
 
-function setFinal(place: "left" | "center" | "right", letterIndex: number) {
-    if (place === "left") {
-        setFinalLeft(listDict["left"].slice(letterIndex, letterIndex + 3));
-    } else if (place === "center") {
-        setFinalCenter(listDict["center"].slice(letterIndex, letterIndex + 3));
-    } else if (place === "right") {
-        setFinalRight(listDict["right"].slice(letterIndex, letterIndex + 3));
-    }
-}
-
-function slotCol(
-    place: "left" | "center" | "right",
-    newStartTime: DOMHighResTimeStamp
-) {
-    const list = listDict[place];
-    const [stopped, setStopped] = useState(false);
-    const [offset, setOffset] = useState(0);
-    const [startTime, setStartTime] = useState(0);
-    if (newStartTime != startTime) {
-        setStopped(false);
-        setStartTime(newStartTime);
-    }
-
-    return (
-        <div
-            style={{
-                animationDuration: String(duration) + "s",
-                transform: stopped ? `translateY(-${offset}%)` : "",
-            }}
-            {...(startTime > 0 &&
-                !stopped && {
-                    onClick: () => {
-                        let letterTime = (duration * 1000) / letters.length;
-                        let elapsedTime = performance.now() - startTime;
-                        let letterIndex =
-                            Math.floor(elapsedTime / letterTime) %
-                            letters.length;
-                        let newOffset = ((letterIndex + 1) / list.length) * 100;
-                        console.log(newOffset);
-                        setOffset(newOffset);
-                        setStopped(true);
-                        setFinal(place, letterIndex);
-                    },
-                })}
-            className={classNames(
-                "slot-container",
-                { stopped: stopped },
-                { started: startTime > 0 }
-            )}
-        >
-            {list.map((letter, index) => (
-                <img
-                    src={"images/letters/" + letter + ".svg"}
-                    alt={"This was the letter " + { letter }}
-                    key={index}
-                />
-            ))}
-        </div>
-    );
-}
-
 export default function PlayPage() {
+    const [listDict, setListDict] = useState({
+        left: newletterList(),
+        center: newletterList(),
+        right: newletterList(),
+    });
+
+    const [finalLeft, setFinalLeft] = useState(listDict["left"].slice(0, 3));
+
+    const [finalCenter, setFinalCenter] = useState(
+        listDict["center"].slice(0, 3)
+    );
+
+    const [finalRight, setFinalRight] = useState(listDict["right"].slice(0, 3));
+
+    function slotCol(
+        place: "left" | "center" | "right",
+        newStartTime: DOMHighResTimeStamp
+    ) {
+        const list = listDict[place];
+        const [stopped, setStopped] = useState(false);
+        const [offset, setOffset] = useState(0);
+        const [startTime, setStartTime] = useState(0);
+
+        function setFinal(
+            place: "left" | "center" | "right",
+            letterIndex: number
+        ) {
+            if (place === "left") {
+                setFinalLeft(
+                    listDict["left"].slice(letterIndex, letterIndex + 3)
+                );
+            } else if (place === "center") {
+                setFinalCenter(
+                    listDict["center"].slice(letterIndex, letterIndex + 3)
+                );
+            } else if (place === "right") {
+                setFinalRight(
+                    listDict["right"].slice(letterIndex, letterIndex + 3)
+                );
+            }
+        }
+
+        if (newStartTime != startTime) {
+            setStopped(false);
+            setStartTime(newStartTime);
+        }
+
+        return (
+            <div
+                style={{
+                    animationDuration: String(duration) + "s",
+                    transform: stopped ? `translateY(-${offset}%)` : "",
+                }}
+                {...(startTime > 0 &&
+                    !stopped && {
+                        onClick: () => {
+                            let letterTime = (duration * 1000) / letters.length;
+                            let elapsedTime = performance.now() - startTime;
+                            let letterIndex =
+                                Math.floor(elapsedTime / letterTime) %
+                                letters.length + 1;
+                            let newOffset =
+                                ((letterIndex) / list.length) * 100;
+                            console.log(newOffset);
+                            setOffset(newOffset);
+                            setStopped(true);
+                            setFinal(place, letterIndex);
+                        },
+                    })}
+                className={classNames(
+                    "slot-container",
+                    { stopped: stopped },
+                    { started: startTime > 0 }
+                )}
+            >
+                {list.map((letter, index) => (
+                    <img
+                        src={"images/letters/" + letter + ".svg"}
+                        alt={"This was the letter " + { letter }}
+                        key={index}
+                    />
+                ))}
+            </div>
+        );
+    }
+
     const [startTime, setStartTime] = useState(0);
 
     return (
